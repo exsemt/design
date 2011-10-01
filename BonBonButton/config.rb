b8d53a224d5cdcef4d664f370af9f7e8604ae690
exsemt <exsemt@gmail.com>
2011-10-01 10:21:45 +0200

    add buttons and menu

---
+++ BonBonButton/config.rb
@@ -0,0 +1,18 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# Indicates whether line comments should be added to compiled css that says where the selectors were defined.
+# Defaults to false in production mode, true in development mode.
+line_comments = false
+
+# The output style for the compiled css. One of: :nested, :expanded, :compact, or :compressed.
+output_style = :nested
+# output_style = :compressed
